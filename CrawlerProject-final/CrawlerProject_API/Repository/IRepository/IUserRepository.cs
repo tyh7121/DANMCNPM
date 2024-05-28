dafe7336ac6648ef9dd7fb51e8103498e176cd8f
thuanan7 <thuanan7@gmail.com>
2024-05-28 13:10:40 +0700

    Implement User Repository

---
+++ CrawlerProject-final/CrawlerProject_API/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using CrawlerProject_API.Models;
+using CrawlerProject_API.Models.DTO;
+
+namespace CrawlerProject_API.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
